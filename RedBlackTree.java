eabe73373f0599da0ff2559712426bfffb48911f
AlexZhang0000 <92870508+AlexZhang0000@users.noreply.github.com>
2025-04-29 19:46:08 -0400

    Update RedBlackTree.java

--- RedBlackTree.java
+++ RedBlackTree.java
@@ -1,34 +1,30 @@
-
 public class RedBlackTree<T extends Comparable<T>> {
-
 
     private enum Color {
         RED, BLACK
     }
 
     private final class Node {
-
         T key;
         Color color;
         Node left, right, parent;
 
         Node(T key) {
             this.key = key;
-            this.color = Color.RED;         
+            this.color = Color.RED;
             this.left = NIL;
             this.right = NIL;
             this.parent = NIL;
         }
     }
 
-    private final Node NIL = new Node(null);    
+    private final Node NIL = new Node(null);
     private Node root = NIL;
 
     public RedBlackTree() {
         NIL.color = Color.BLACK;
         NIL.left = NIL.right = NIL.parent = NIL;
     }
-
 
     public boolean contains(T key) {
         return searchNode(key) != NIL;
@@ -38,9 +34,7 @@ public class RedBlackTree<T extends Comparable<T>> {
         Node current = root;
         while (current != NIL) {
             int cmp = key.compareTo(current.key);
-            if (cmp == 0) {
-                return current;
-            }
+            if (cmp == 0) return current;
             current = (cmp < 0) ? current.left : current.right;
         }
         return NIL;
@@ -55,6 +49,7 @@ public class RedBlackTree<T extends Comparable<T>> {
             y = x;
             x = key.compareTo(x.key) < 0 ? x.left : x.right;
         }
+
         z.parent = y;
         if (y == NIL) {
             root = z;
@@ -63,24 +58,24 @@ public class RedBlackTree<T extends Comparable<T>> {
         } else {
             y.right = z;
         }
+
         insertFix(z);
     }
 
     private void insertFix(Node z) {
         while (z.parent.color == Color.RED) {
             if (z.parent == z.parent.parent.left) {
-                Node y = z.parent.parent.right; 
-                if (y.color == Color.RED) {           
+                Node y = z.parent.parent.right;
+                if (y.color == Color.RED) {
                     z.parent.color = Color.BLACK;
                     y.color = Color.BLACK;
                     z.parent.parent.color = Color.RED;
                     z = z.parent.parent;
                 } else {
-                    if (z == z.parent.right) {      
+                    if (z == z.parent.right) {
                         z = z.parent;
                         leftRotate(z);
                     }
-                    // Case 3
                     z.parent.color = Color.BLACK;
                     z.parent.parent.color = Color.RED;
                     rightRotate(z.parent.parent);
@@ -142,6 +137,114 @@ public class RedBlackTree<T extends Comparable<T>> {
         y.parent = x;
     }
 
+    public void delete(T key) {
+        Node z = searchNode(key);
+        if (z == NIL) return;
+
+        Node y = z;
+        Color yOriginalColor = y.color;
+        Node x;
+
+        if (z.left == NIL) {
+            x = z.right;
+            transplant(z, z.right);
+        } else if (z.right == NIL) {
+            x = z.left;
+            transplant(z, z.left);
+        } else {
+            y = minimum(z.right);
+            yOriginalColor = y.color;
+            x = y.right;
+            if (y.parent == z) {
+                x.parent = y;
+            } else {
+                transplant(y, y.right);
+                y.right = z.right;
+                y.right.parent = y;
+            }
+            transplant(z, y);
+            y.left = z.left;
+            y.left.parent = y;
+            y.color = z.color;
+        }
+
+        if (yOriginalColor == Color.BLACK) {
+            deleteFix(x);
+        }
+    }
+
+    private void deleteFix(Node x) {
+        while (x != root && x.color == Color.BLACK) {
+            if (x == x.parent.left) {
+                Node w = x.parent.right;
+                if (w.color == Color.RED) {
+                    w.color = Color.BLACK;
+                    x.parent.color = Color.RED;
+                    leftRotate(x.parent);
+                    w = x.parent.right;
+                }
+                if (w.left.color == Color.BLACK && w.right.color == Color.BLACK) {
+                    w.color = Color.RED;
+                    x = x.parent;
+                } else {
+                    if (w.right.color == Color.BLACK) {
+                        w.left.color = Color.BLACK;
+                        w.color = Color.RED;
+                        rightRotate(w);
+                        w = x.parent.right;
+                    }
+                    w.color = x.parent.color;
+                    x.parent.color = Color.BLACK;
+                    w.right.color = Color.BLACK;
+                    leftRotate(x.parent);
+                    x = root;
+                }
+            } else {
+                Node w = x.parent.left;
+                if (w.color == Color.RED) {
+                    w.color = Color.BLACK;
+                    x.parent.color = Color.RED;
+                    rightRotate(x.parent);
+                    w = x.parent.left;
+                }
+                if (w.right.color == Color.BLACK && w.left.color == Color.BLACK) {
+                    w.color = Color.RED;
+                    x = x.parent;
+                } else {
+                    if (w.left.color == Color.BLACK) {
+                        w.right.color = Color.BLACK;
+                        w.color = Color.RED;
+                        leftRotate(w);
+                        w = x.parent.left;
+                    }
+                    w.color = x.parent.color;
+                    x.parent.color = Color.BLACK;
+                    w.left.color = Color.BLACK;
+                    rightRotate(x.parent);
+                    x = root;
+                }
+            }
+        }
+        x.color = Color.BLACK;
+    }
+
+    private void transplant(Node u, Node v) {
+        if (u.parent == NIL) {
+            root = v;
+        } else if (u == u.parent.left) {
+            u.parent.left = v;
+        } else {
+            u.parent.right = v;
+        }
+        v.parent = u.parent;
+    }
+
+    private Node minimum(Node node) {
+        while (node.left != NIL) {
+            node = node.left;
+        }
+        return node;
+    }
 
     public String toJson() {
         return toJson(root);
@@ -160,9 +263,7 @@ public class RedBlackTree<T extends Comparable<T>> {
     }
 
     private void inorderPrint(Node n) {
-        if (n == NIL) {
-            return;
-        }
+        if (n == NIL) return;
         inorderPrint(n.left);
         System.out.print(n.key + (n.color == Color.RED ? "R " : "B "));
         inorderPrint(n.right);
